330ddfc466ea61321acc4cd35ab4b9910c8c9e22
Ayo Onamusi <onamusia@gmail.com>
2023-06-29 23:05:27 +0100

    Movie Details Page rendering.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HomePage from "./pages/homePage";
+// import HomePage from "./pages/homePage"; // commented out in lab 1 
+import MovieDetailsPage from './pages/movieDetailsPage'
+
 import sample from './stories/sampleData'
 
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
+// commented out in lab 1 and replaced with the one below it 
+// const App = () => {
+//   return (
+//       <HomePage movies={movies} />
+//   );
+// };
+
 const App = () => {
   return (
-      <HomePage movies={movies} />
+    <MovieDetailsPage movie={sample} images={images} />
   );
 };
+
 
 const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
